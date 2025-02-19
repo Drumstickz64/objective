949f50b44e8c7e2e1c3aff70dae06e3e359e6839
Drumstickz64 <gearman533@gmail.com>
2025-02-19 11:48:51 +0300

    add face parsing

--- src/bin/playground.rs
+++ src/bin/playground.rs
@@ -1,8 +1,29 @@
 use std::fs;
 
-use objective::parse_obj;
+use objective::{parse_obj, Mesh};
 
 fn main() {
     let model = parse_obj(fs::read_to_string("assets/box.obj").unwrap().as_str()).unwrap();
-    dbg!(model);
+
+    print_mesh(&model.meshes[0]);
+}
+
+fn print_mesh(mesh: &Mesh) {
+    for (i, vertex) in mesh.vertices.chunks(3).enumerate() {
+        println!("vertex {} => {vertex:?}", i + 1);
+    }
+
+    let mut prim_i = 0;
+    let mut prim_num = 1;
+    while prim_i < mesh.primitives.len() {
+        let length = mesh.primitives[prim_i];
+        println!(
+            "primitive {} => {:?}",
+            prim_num,
+            &mesh.primitives[prim_i + 1..prim_i + 1 + length],
+        );
+
+        prim_i += length + 1;
+        prim_num += 1;
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,22 @@
-use glam::Vec3;
+#[derive(Debug, Clone, Default, PartialEq)]
+pub struct Model {
+    pub meshes: Vec<Mesh>,
+}
+
+#[derive(Debug, Clone, Default, PartialEq)]
+pub struct Mesh {
+    pub vertices: Vec<f32>,
+    /// a flat array of a length, followed by a series of indices of that length
+    /// e.g. 3 1 2 3 1 4 2 5 6
+    pub primitives: Vec<usize>,
+}
 
 #[derive(Debug, Clone, PartialEq, Hash)]
 pub struct ObjParseError;
 
 pub fn parse_obj(content: &str) -> Result<Model, ObjParseError> {
     let mut vertices = Vec::new();
+    let mut primitives = Vec::new();
 
     for (line_number, line) in content
         .lines()
@@ -18,7 +30,15 @@ pub fn parse_obj(content: &str) -> Result<Model, ObjParseError> {
             .expect("expected statement type at start of line, but got nothing");
 
         match stmt_type {
-            "v" => vertices.push(parse_position(segments, line_number).unwrap()),
+            "v" => parse_vec3(&mut vertices, segments, line_number).unwrap(),
+            "f" => {
+                let num_indices =
+                    parse_primitive(&mut primitives, vertices.len() / 3, segments, line_number)
+                        .unwrap();
+                if num_indices < 3 {
+                    panic!("{line}: 'f' command must provide at least 3 indices, but instead provided '{num_indices}");
+                }
+            }
             stmt_type => eprintln!(
                 "WARNING: unknown statement type '{}', skipping...",
                 stmt_type
@@ -28,34 +48,50 @@ pub fn parse_obj(content: &str) -> Result<Model, ObjParseError> {
 
     // handle 1 mesh currently
     Ok(Model {
-        meshes: vec![Mesh { vertices }],
+        meshes: vec![Mesh {
+            vertices,
+            primitives,
+        }],
     })
 }
 
-fn parse_position<'a>(
-    segments: impl Iterator<Item = &'a str>,
-    line: usize,
-) -> Result<Vec3, ObjParseError> {
-    parse_vec3(segments, line)
-}
-
 fn parse_vec3<'a>(
+    data: &mut Vec<f32>,
     mut segments: impl Iterator<Item = &'a str>,
     _line: usize,
-) -> Result<Vec3, ObjParseError> {
-    let x = segments.next().unwrap().parse::<f32>().unwrap();
-    let y = segments.next().unwrap().parse::<f32>().unwrap();
-    let z = segments.next().unwrap().parse::<f32>().unwrap();
+) -> Result<(), ObjParseError> {
+    data.push(segments.next().unwrap().parse::<f32>().unwrap());
+    data.push(segments.next().unwrap().parse::<f32>().unwrap());
+    data.push(segments.next().unwrap().parse::<f32>().unwrap());
 
-    Ok(Vec3::new(x, y, z))
+    Ok(())
 }
 
-#[derive(Debug, Clone, Default, PartialEq)]
-pub struct Model {
-    meshes: Vec<Mesh>,
-}
+fn parse_primitive<'a>(
+    primitives: &mut Vec<usize>,
+    num_vertices: usize,
+    segments: impl Iterator<Item = &'a str>,
+    _line: usize,
+) -> Result<usize, ObjParseError> {
+    let mut length = 0;
 
-#[derive(Debug, Clone, Default, PartialEq)]
-pub struct Mesh {
-    pub vertices: Vec<Vec3>,
+    let length_index = primitives.len();
+    primitives.push(0); // zero length for now, will be updated at end of primitive parsing
+
+    for index in segments {
+        let index: i64 = index.parse().unwrap();
+        let index = match index {
+            // + index instead of - index because it's negative
+            // + 1 because indices are 1 based
+            ..0 => (num_vertices as i64 + index + 1) as usize,
+            0 => panic!("indices in a primitive must not be 0"),
+            1.. => index as usize,
+        };
+        primitives.push(index);
+        length += 1;
+    }
+
+    primitives[length_index] = length;
+
+    Ok(length)
 }
